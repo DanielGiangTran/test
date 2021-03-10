81dda6099b40c744e1e17558a14428dfc023e3b8
DanielGiangTran <62652860+DanielGiangTran@users.noreply.github.com>
2021-03-11 11:21:50 +1300

    Delete SpringBootHSQLPrj directory

--- SpringBootHSQLPrj/src/main/java/nz/ac/op/cs/SpringBootHSQLPrj/db/QuizUserRepository.java
+++
@@ -1,8 +0,0 @@
-package nz.ac.op.cs.SpringBootHSQLPrj.db;
-
-
-import nz.ac.op.cs.SpringBootHSQLPrj.models.QuizUser;
-import org.springframework.data.repository.CrudRepository;
-
-public interface QuizUserRepository  extends CrudRepository<QuizUser, String>{
-}
